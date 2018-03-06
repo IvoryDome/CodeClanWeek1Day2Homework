26dd48a62ceaaec4b745ddbc68155207d10b4795
IvoryDome <36415614+IvoryDome@users.noreply.github.com>
2018-03-06 21:39:42 +0000

    Add files via upload

---
+++ cc_w1d2_functions-1/start_point/specs/ruby_functions_practice.rb
@@ -0,0 +1,62 @@
+def return_10
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  string = "A string of length 21".length
+  return string
+end
+
+def join_string(string_1, string_2)
+  string_1 = "Mary had a little lamb, "
+  string_2 = "it's fleece was white as snow"
+return string_1 + string_2
+end
+
+def add_string_as_number(first_number, second_number)
+return first_number.to_i + second_number.to_i
+end
+
+def number_to_full_month_name(month)
+  result = case month
+  when 1
+      "January"
+    when 3
+      "March"
+    when 9
+      "September"
+    else
+      return result
+  end
+end
+
+
+
+def number_to_short_month_name(month)
+  result = case month
+  when 1
+      "Jan"
+    when 3
+      "Mar"
+    when 9
+      "Sep"
+    else
+      return result
+  end
+end
